6e8704395110730e28212e7d3f0a8cfa21c88de1
devinjburch <devinjburch@gmail.com>
2026-01-20 13:26:42 -0700

    Completed Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 11);
+        int number;
+
+        Console.WriteLine("What is the magic number?");
+        Console.WriteLine("What is your guess?");
+        number = int.Parse(Console.ReadLine());
+
+        while (number != magic_number)
+        {
+            Console.WriteLine("That's not it. Try again!?");
+            Console.WriteLine("What is your guess?");
+            number = int.Parse(Console.ReadLine());
+        
+
+        if (number > magic_number)
+        {
+            Console.WriteLine("Too high! Try again!?");
+            Console.WriteLine("What is your guess?");
+            number = int.Parse(Console.ReadLine());
+        }
+        else if (number < magic_number)
+        {
+            Console.WriteLine("Too low! Try again!?");
+            Console.WriteLine("What is your guess?");
+            number = int.Parse(Console.ReadLine());
+        }
+        else
+            {
+            Console.WriteLine("You got it!");
+            }
+        }
     }
 }
