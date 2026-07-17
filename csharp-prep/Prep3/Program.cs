aa668b0d75e9d6024d3e022f3b06ae5aafcc5fb6
unknown <devinjburch@gmail.com>
2026-07-16 18:03:53 -0600

    Initial commit

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -9,33 +9,30 @@ class Program
         int magic_number = randomGenerator.Next(1, 11);
         int number;
 
-        Console.WriteLine("What is the magic number?");
+        Console.WriteLine("Guess the magic number!");
         Console.WriteLine("What is your guess?");
         number = int.Parse(Console.ReadLine());
 
         while (number != magic_number)
         {
-            Console.WriteLine("That's not it. Try again!?");
-            Console.WriteLine("What is your guess?");
-            number = int.Parse(Console.ReadLine());
-        
-
-        if (number > magic_number)
-        {
-            Console.WriteLine("Too high! Try again!?");
-            Console.WriteLine("What is your guess?");
-            number = int.Parse(Console.ReadLine());
-        }
-        else if (number < magic_number)
-        {
-            Console.WriteLine("Too low! Try again!?");
-            Console.WriteLine("What is your guess?");
-            number = int.Parse(Console.ReadLine());
-        }
-        else
+ 
+            if (number > magic_number)
             {
-            Console.WriteLine("You got it!");
+                Console.WriteLine("Too high! Try again!?");
+                Console.WriteLine("What is your guess?");
+                number = int.Parse(Console.ReadLine());
             }
+            else if (number < magic_number)
+            {
+                Console.WriteLine("Too low! Try again!?");
+                Console.WriteLine("What is your guess?");
+                number = int.Parse(Console.ReadLine());
+            }
+            
+               
         }
+        
+        Console.WriteLine("You got it!");
+        
     }
 }
