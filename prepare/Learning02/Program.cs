0e9e20c2dcc79fa6d0ffd0a549b8683d76bc84e8
unknown <devinjburch@gmail.com>
2026-07-16 18:50:11 -0600

    Initial commit

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,28 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job firstJob = new Job();
+
+        firstJob._jobTitle = "Software Developer";
+        firstJob._company = "Google";
+        firstJob._startYear = 2020;
+        firstJob._endYear = 2022;
+
+        Job secondJob = new Job();
+
+        secondJob._jobTitle = "Project Manager";
+        secondJob._company = "Amazon";
+        secondJob._startYear = 2022;
+        secondJob._endYear = 2024;
+
+        Resume resume = new Resume();
+
+        resume._name = "Jordan Smith";
+        resume._jobs.Add(firstJob);
+        resume._jobs.Add(secondJob);
+
+        resume.Display();
     }
 }
